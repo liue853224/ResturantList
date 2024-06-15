bf5cc1e4d9390d20e054e9fefe9625732e19cceb
liue853224@gmail.com <liue853224@gmail.com>
2024-06-15 23:13:24 +0800

    create route and layouts pages

--- app.js
+++ app.js
@@ -2,9 +2,25 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const { engine } = require("express-handlebars");
+const restaurants = require("./public/jsons/restaurant.json").results;
 
 // 設置
 app.use(express.static("public"));
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
+app.set("views", "./views");
+
+//route
+app.get("/", (req, res) => {
+  res.redirect("/Restaurants");
+});
+
+app.get("/Restaurants", (req, res) => {
+  res.render("index");
+});
+
+app.get("/Restaurants/:id", (req, res) => {});
 
 // Listen
 app.listen(port, () => {
