99ed6af92a40264fa980a9266c78eed40b217c9b
liue853224@gmail.com <liue853224@gmail.com>
2024-08-07 08:01:07 +0800

    init user route

--- routes/users.js
+++ routes/users.js
@@ -5,4 +5,24 @@ const router = express.Router();
 const db = require("./models");
 const users = db.users;
 
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
+router.post("/login", (req, res) => {
+  res.send("send user data");
+});
+
+router.get("/register", (req, res) => {
+  res.render("register");
+});
+
+router.post("/register", (req, res) => {
+  res.send("create user data");
+});
+
+router.get("/logout", (req, res) => {
+  res.send("logout user");
+});
+
 module.exports = router;
